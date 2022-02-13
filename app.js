ac434eee1f9fc861187fee34714e31241f99cbd4
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-02-14 02:42:13 +0530

    feat: changes correspond to Bulma framework

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ p1Button.addEventListener('click', function() {
         p1Scores += 1;
         if (p1Scores === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Scores;
 
@@ -31,8 +33,10 @@ p2Button.addEventListener('click', function() {
         p2Scores += 1;
         if (p2Scores === winningScore) {
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Scores;
 
@@ -55,6 +59,8 @@ function reset() {
     p2Scores = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('loser', 'winner');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-danger', 'has-text-success');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
